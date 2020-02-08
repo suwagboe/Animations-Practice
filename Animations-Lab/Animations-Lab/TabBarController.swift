c54f94876cac19ce812ca0f2008140752b953ed0
uwagboe <sm.uwagboe@gmail.com>
2020-02-07 19:17:53 -0500

    added the window scene in the scene delgate and made the root view controller  the tabBarController

---
+++ Animations-Lab/Animations-Lab/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Animations-Lab
+//
+//  Created by Pursuit on 2/7/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
